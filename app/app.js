7e895f4cb9d59284e83ace82cc15fd7e1bf42315
David Rodgers <dtrodger20@gmail.com>
2016-11-20 16:48:43 -0600

    Add two way data binding

--- app/app.js
+++ app/app.js
@@ -1,22 +1,6 @@
 // MODULE is a container for different parts of your app
 // second arguement defines dependencies
 
-var myApp = angular.module("myApp", []);
-
-// CONTROLER is a JS function w/ job to build a model for the view to display
-// register controller to app
-// data attached to the scope is the model
-
-myApp.controller('myController', ['$scope', function($scope) {
-	var employee = {
-		firstName: "David",
-		lastName: "Rodgers",
-		gender: "Male"
-	}
-
-	$scope.employee = employee;
-}]);
-
 // create a module, controller and register controller to module in one line - method chaining
 
 // var myApp = angular
@@ -30,6 +14,25 @@ myApp.controller('myController', ['$scope', function($scope) {
 
 // 	$scope.employee = employee;
 // }]);
+
+var myApp = angular.module("myApp", []);
+
+// CONTROLER is a JS function w/ job to build a model for the view to display
+// register controller to app
+// data attached to the scope is the model
+
+myApp.controller('myController', ['$scope', function($scope) {
+
+	var employee = {
+		firstName: "David",
+		lastName: "Rodgers",
+		gender: "Male",
+		image_src: "../content/images/space_monkey.jpg"
+	};
+
+	$scope.employee = employee;
+	
+}]);
 
 
 
