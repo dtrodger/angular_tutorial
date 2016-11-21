1af1ba694e25b37abffd8695209cb9811721a923
David Rodgers <dtrodger20@gmail.com>
2016-11-20 19:30:33 -0600

    ajax

--- app/app.js
+++ app/app.js
@@ -1,41 +1,51 @@
 // MODULE is a container for different parts of your app
 // second arguement defines dependencies
 
-// create a module, controller and register controller to module in one line - method chaining
-
-// var myApp = angular
-// 	.module('myApp', [])
-// 	.controller('myController', ['$scope', function($scope) {
-// 	var employee = {
-// 		firstName: "David",
-// 		lastName: "Rodgers",
-// 		gender: "Male"
-// 	}
-
-// 	$scope.employee = employee;
-// }]);
-
 var myApp = angular.module("myApp", []);
 
 // CONTROLER is a JS function w/ job to build a model for the view to display
 // register controller to app
 // data attached to the scope is the model
 
-myApp.controller('employeeDirectory', ['$scope', '$http', function($scope, $http) {
+myApp.controller('employeeDirectory', ['$scope', '$http', '$log', function($scope, $http, $log) {
 
-	$http.get('../data/employees.json').success(function(JSON){
-		$scope.employees = JSON;
-	})
+	// AJAX
+	// $http.get, .put, .post ect...
+
+	// handle response from an API... 
+	// function(response) {
+	// 	$scope.attribute = response.data
+	// }
+
+	// $http.get("../data/employees.json").success(function(JSON){
+	// 	$scope.employees = JSON;
+	// })
+
+	var successCallBack = function (response) {
+		// $scope.employees = response.data if requested from API instead of local JSON
+		$scope.employees = response;
+	};
+
+	var errorCallBack = function (response) {
+		$scope.error = response.data;
+		// console logs the response
+		$log.info(response);
+	};
+
+	$http({
+		method: "GET",
+		url: "../data/employees.json"
+	}).success(successCallBack, errorCallBack)
 
 	$scope.orderBy = "firstName";
 
 	$scope.directoryView = "directory_list.html";
 
-	$scope.upvote = function(employee) {
+	$scope.upvote = function (employee) {
 		employee.votes++;
 	};
 
-	$scope.downvote = function(employee) {
+	$scope.downvote = function (employee) {
 		employee.votes--;
 	};
 
